5f37f92ae8700ef1e9ce96a82390894624845d95
Mozola-satlawa <misia.filipek@gmail.com>
2025-08-12 11:58:33 +0200

    Poprawa plików index, headers i middleware

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,20 +1,27 @@
 export const onRequest = async ({ request, next }) => {
-  const auth = request.headers.get('Authorization') || '';
-  const expectedUser = 'user';
-  const expectedPass = 'Arkon2025';
-  const challenge = new Response('Authentication required', {
-    status: 401,
-    headers: { 'WWW-Authenticate': 'Basic realm="Czyste Powietrze"' }
-  });
+    const auth = request.headers.get('Authorization') || '';
+    const expectedUser = 'user';
+    const expectedPass = 'Arkon2025';
 
-  if (!auth.startsWith('Basic ')) return challenge;
+    const challenge = new Response('Authentication required', {
+        status: 401,
+        headers: {
+            'WWW-Authenticate': 'Basic realm="Czyste Powietrze"'
+        }
+    });
 
-  try {
-    const [user, pass] = atob(auth.slice(6)).split(':');
-    if (user === expectedUser && pass === expectedPass) {
-      return next();
+    if (!auth.startsWith('Basic ')) {
+        return challenge;
     }
-  } catch (e) {}
 
-  return challenge;
+    try {
+        const [user, pass] = atob(auth.slice(6)).split(':');
+        if (user === expectedUser && pass === expectedPass) {
+            return next();
+        }
+    } catch (e) {
+        console.error(e);
+    }
+
+    return challenge;
 };
